0c9e0c3067b113dd8e66ef15374be501edc86fde
Shub562 <shubhampandey820@gmail.com>
2024-09-02 16:15:29 -0500

    Added static templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,23 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -17,10 +27,12 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields=['-publish'])
+            models.Index(fields=['-publish']),
         ]
     def __str__(self):
         return self.title
